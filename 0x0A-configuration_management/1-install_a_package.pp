2de1b730acf30536d475294ce3af2a58fbc6c8df
eungobs <eungobs@gmail.com>
2023-11-24 12:07:00 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install puppet-lint
-package{'puppet-lint':
-ensure => '2.5.0',
-provider => 'gem'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0"',
+  require => Package['python3-pip'],
 }

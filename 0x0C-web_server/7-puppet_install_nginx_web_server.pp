e8ae63b10d56e90a8105983c70c0bb4476711d6e
eungobs <eungobs@gmail.com>
2023-12-09 08:34:49 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,24 @@
-# install and configure an Nginx server using Puppet instead of Bash
+# Script to install nginx using puppet
 
-package { 'nginx':
-  ensure => installed,
-  name   => 'nginx'
+package {'nginx':
+  ensure => 'present',
 }
-file { 'path_to_html':
-  content => 'Holberton School',
-  path    => '/var/www/html/index.nginx-debian.html'
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 }
-file_line { 'put_in_line':
-  ensure => present,
-  path   => 'etc/nginx/sites-available/default',
-  after  => '/listen 80 default_server;'
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+  provider => shell,
 }
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx']
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/;\n\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }

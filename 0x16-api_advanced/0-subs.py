fe8dce4dcebc2b5cda5419565ba1519243cf918b
eungobs <eungobs@gmail.com>
2024-01-16 20:16:35 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+number of subscribers for a given subreddit
 """
-import requests
+
+from requests import get
+
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
-    user_agent = 'Mozilla/5.0'
-    headers = {'User-Agent': user_agent}
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        data = response.json()['data']
-        subscribers_count = data['subscribers']
-        return subscribers_count
-    except (KeyError, ValueError):
-        return 0  # Return 0 if there is an issue with the response or parsing
+        return results.get('data').get('subscribers')
 
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py subreddit")
-        sys.exit(1)
-
-    subreddit_name = sys.argv[1]
-    subscribers_count = number_of_subscribers(subreddit_name)
-    
-    print(subscribers_count)
+    except Exception:
+        return 0

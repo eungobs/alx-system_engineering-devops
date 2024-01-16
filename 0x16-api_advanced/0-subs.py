f1a935483895b8a8f7b11dbf2b0c08b383bddb89
eungobs <eungobs@gmail.com>
2024-01-16 16:00:38 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
+"""
+Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
-        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers, allow_redi    rects=False)
-    if r.status_code >= 300:
+    
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        
+        if response.status_code == 200:
+            data = response.json()
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
+        else:
+            return 0
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
         return 0
-    json = r.json()
-    data_dict = json.get('data')
-    return(data_dict.get('subscribers'))
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py subreddit")
+        sys.exit(1)
+
+    subreddit_name = sys.argv[1]
+    subscribers_count = number_of_subscribers(subreddit_name)
+    
+    print(subscribers_count)

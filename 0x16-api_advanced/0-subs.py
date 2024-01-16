cd9abd2696ee8ad0c5f76d6a44f2f54fa434d287
eungobs <eungobs@gmail.com>
2024-01-16 16:03:49 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,40 +1,16 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        
-        if response.status_code == 200:
-            data = response.json()
-            subscribers_count = data['data']['subscribers']
-            return subscribers_count
-        else:
-            return 0
-
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py subreddit")
-        sys.exit(1)
-
-    subreddit_name = sys.argv[1]
-    subscribers_count = number_of_subscribers(subreddit_name)
-    
-    print(subscribers_count)
+    results = response.json().get("data")
+    return results.get("subscribers")
